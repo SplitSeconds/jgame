5ee0d5b5e2effb9a9f4a1748fa23395a2a3e750b
Nele Luepkes <nluepkes@gmail.com>
2018-10-17 17:58:10 +0200

    implement jquery

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,37 @@
 console.log('Hello Berlin!')
 
 function goToPage(link){
-  //Goal: hide all the divs with data-page(jQuery) , except the right one
+//Goal: hide all the divs with data-page(jQuery) , except the right one
+
+//Solution 1
+// $('[data-page').hide();
+// $('[data-page='+link+']').show();
+
+//Solution 2
+$('[data-page').each(function() {
+  var page = $(this).data('page')
+  if (page === link)
+    $(this).show()
+  else 
+    $(this).hide()
+
+  //console.log($(this).data('page'));
+
+  //equals line above
+  //console.log($(this).attr('data-page'));
+
+  //BONUS Add class active in the navbar
+  $('li.nav-item').each(function(){
+    var href =  $(this).find('a.nav-link').attr('href')  
+    console.log(href);
+    if (href === link){
+      $(this).addClass('active')
+    else
+      $(this).removeClass('active')
+    }
+
+  })
+});
 }
 
 // go to main
@@ -9,3 +39,12 @@ goToPage('play')
 
 // Listen for click events on <a>
 //Hint: event.preventDefault
+
+$('a').click(function(event){
+event.preventDefault()
+var href = $(this).attr('href')
+goToPage(href)
+})
+
+
+
