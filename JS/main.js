13e1db26f011f843a3c476a937dabc63e06b123a
Nele Luepkes <nluepkes@gmail.com>
2018-10-17 17:04:36 +0200

    Add page structure

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,11 @@
 console.log('Hello Berlin!')
+
+function goToPage(link){
+  //Goal: hide all the divs with data-page(jQuery) , except the right one
+}
+
+// go to main
+goToPage('play')
+
+// Listen for click events on <a>
+//Hint: event.preventDefault
